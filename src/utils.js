80ae514b0b99074cb04ae47ff97f565f52020a6e
Daniel <daniel.mutembei@student.moringaschool.com>
2025-01-28 15:12:59 +0300

    done

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,15 @@
 // Your code here
+function isPalindrome(word) {
+    if (typeof word !== 'string') {
+        throw new Error('Input must be a string');
+    }
+    if (!/^[A-Za-z]+$/.test(word)) {
+        throw new Error('Input must contain only alphabetic characters');
+    }
+
+    const lowerCasedWord = word.toLowerCase();
+    const reversedWord = lowerCasedWord.split('').reverse().join('');
+    return lowerCasedWord === reversedWord;
+}
+
+module.exports = { isPalindrome };
